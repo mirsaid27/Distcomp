cbbabb56e567efcd69e11a088cc3b5cd0cc81dd6
MARUF <MARUF@DESKTOP-CNK3C8V>
2024-05-04 18:01:40 +0300

    remove dummy files

--- Lab4.Discussion/DTO/ResponseDTO/NoteResponseDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Lab4.Discussion.DTO.ResponseDTO
-{
-    public class NoteResponseDto
-    {
-        public long Id { get; set; }
-        public long NewsId { get; set; }
-        public string Content { get; set; }
-    }
-}
