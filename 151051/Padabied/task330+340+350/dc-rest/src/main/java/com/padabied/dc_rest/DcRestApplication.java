4c644e9595bfd199208deb823257d54570f99228
Padabied <57150003+Padabied@users.noreply.github.com>
2024-10-24 21:44:30 +0300

    Add files via upload

---
+++ 151051/Padabied/task330+340+350/dc-rest/src/main/java/com/padabied/dc_rest/DcRestApplication.java
@@ -0,0 +1,15 @@
+package com.padabied.dc_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+@SpringBootApplication
+@EnableCaching
+public class DcRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DcRestApplication.class, args);
+	}
+
+}
