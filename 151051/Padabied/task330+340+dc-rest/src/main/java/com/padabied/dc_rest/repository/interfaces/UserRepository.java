2125dd58da234cb1f158d8001204dc9e43a2fa61
Padabied <57150003+Padabied@users.noreply.github.com>
2024-10-24 21:40:18 +0300

    Delete 151051/Padabied/task330+340+dc-rest directory

--- 151051/Padabied/task330+340+dc-rest/src/main/java/com/padabied/dc_rest/repository/interfaces/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.padabied.dc_rest.repository.interfaces;
-
-import com.padabied.dc_rest.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository <User, Long> {
-}
