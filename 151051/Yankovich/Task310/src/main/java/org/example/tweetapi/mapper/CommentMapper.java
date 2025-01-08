91386cc9a1ce9195ffd47ae7d022204c53326b60
Golovolastik <yankovich.aliaksei@gmail.com>
2025-01-08 11:37:25 +0300

    feature: pass author tests

--- 151051/Yankovich/Task310/src/main/java/org/example/tweetapi/mapper/CommentMapper.java
+++ 151051/Yankovich/Task310/src/main/java/org/example/tweetapi/mapper/CommentMapper.java
@@ -1,4 +1,17 @@
 package org.example.tweetapi.mapper;
 
+import org.example.tweetapi.model.dto.request.CommentRequestTo;
+import org.example.tweetapi.model.dto.response.CommentResponseTo;
+import org.example.tweetapi.model.entity.Comment;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
 public interface CommentMapper {
+
+    @Mapping(source = "tweet.id", target = "tweetId")
+    CommentResponseTo toResponse(Comment comment);
+
+    @Mapping(target = "id", ignore = true)
+    Comment toEntity(CommentRequestTo commentRequestDto);
 }
