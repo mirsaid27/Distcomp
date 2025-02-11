e36ee11e55a9ba83ee37f6af04e14984eb72cc8c
vlados1kin <lashkin2004@gmail.com>
2025-02-12 00:48:26 +0300

    Configure repositories

---
+++ 251001/Lashkin/src/Infrastructure/Persistence/Extensions/ServiceExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Persistence.Repositories;
+
+namespace Persistence.Extensions;
+
+public static class ServiceExtensions
+{
+    public static IServiceCollection ConfigureRepositories(this IServiceCollection services)
+    {
+        services.AddDbContext<RepositoryContext>(options => options.UseInMemoryDatabase("DistcompDB"));
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+        
+        return services;
+    }
+}
--- 251001/Lashkin/src/Presentation/API/Program.cs
+++ 251001/Lashkin/src/Presentation/API/Program.cs
@@ -1,7 +1,11 @@
+using Persistence.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.ConfigureRepositories();
 
 var app = builder.Build();
 
