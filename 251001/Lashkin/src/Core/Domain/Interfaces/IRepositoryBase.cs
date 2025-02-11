91db696e550dfdc1fc03a280bd3fe30f270594e3
vlados1kin <lashkin2004@gmail.com>
2025-02-12 00:47:24 +0300

    Add repositories

---
+++ 251001/Lashkin/src/Core/Domain/Interfaces/IRepositoryBase.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+
+namespace Domain.Interfaces;
+
+public interface IRepositoryBase<T>
+{
+    IQueryable<T> FindAll(bool trackChanges);
+    IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression, bool trackChanges);
+    Task<T?> FindByIdAsync(long id, CancellationToken cancellationToken = default);
+    Task CreateAsync(T entity);
+    void Update(T entity);
+    void Delete(T entity);
+}
---
+++ 251001/Lashkin/src/Core/Domain/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Interfaces;
+
+public interface IUnitOfWork
+{
+    IUserRepository User { get; }
+    INewsRepository News { get; }
+    INoticeRepository Notice { get; }
+    ILabelRepository Label { get; }
+    Task SaveChangesAsync(CancellationToken cancellationToken = default);
+}
