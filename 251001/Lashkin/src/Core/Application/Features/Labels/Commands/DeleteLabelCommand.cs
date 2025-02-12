a7e1df1a48b477ad0e052cc5c069b6449eadb2e7
vlados1kin <lashkin2004@gmail.com>
2025-02-12 12:57:21 +0300

    Create commands, queries and mapping profiles

---
+++ 251001/Lashkin/src/Core/Application/Features/Labels/Commands/DeleteLabelCommand.cs
@@ -0,0 +1,6 @@
+﻿using Application.DTO.Response;
+using MediatR;
+
+namespace Application.Features.Labels.Commands;
+
+public record DeleteLabelCommand(long Id) : IRequest<LabelResponseTo>;
---
+++ 251001/Lashkin/src/Core/Application/Features/Users/Commands/CreateUserCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using Application.DTO.Response;
+using AutoMapper;
+using Domain.Entities;
+using Domain.Interfaces;
+using MediatR;
+
+namespace Application.Features.Users.Commands;
+
+public class CreateUserCommandHandler : IRequestHandler<CreateUserCommand, UserResponseTo>
+{
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IMapper _mapper;
+
+    public CreateUserCommandHandler(IUnitOfWork unitOfWork, IMapper mapper)
+    {
+        _unitOfWork = unitOfWork;
+        _mapper = mapper;
+    }
+
+    public async Task<UserResponseTo> Handle(CreateUserCommand request, CancellationToken cancellationToken)
+    {
+        var user = _mapper.Map<User>(request.UserRequestTo);
+
+        _unitOfWork.User.Create(user);
+
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+        var userResponseTo = _mapper.Map<UserResponseTo>(user);
+
+        return userResponseTo;
+    }
+}
