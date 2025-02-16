ddedf13ee11472b14fc9f027baf7e684acb30b87
significantnose <yanucevich.d@gmail.com>
2025-02-17 01:38:14 +0300

    [task2/refactor]: recommended by npgsql approach to accessing the database implemented

--- 251002/Yanucevich/Infrastructure/Repositories/Postgres/MarkerRepositoryPg.cs
+++ 251002/Yanucevich/Infrastructure/Repositories/Postgres/MarkerRepositoryPg.cs
@@ -3,6 +3,7 @@ using Dapper;
 using Domain.Models;
 using Domain.Repositories;
 using Domain.Shared;
+using Infrastructure.Errors;
 using Infrastructure.Repositories.Interfaces;
 using Infrastructure.Settings;
 using Microsoft.Extensions.Options;
@@ -30,14 +31,20 @@ public class MarkerRepositoryPg : PgRepository, IMarkerRepository
         """;
 
         await using var connection = await GetConnection();
-        long markerId = await connection.QuerySingleAsync<long>(
-            new CommandDefinition(
-                sqlInsertMarker,
-                marker
-            )
-        );
+        await using var cmd = new NpgsqlCommand(sqlInsertMarker, connection);
+        cmd.Parameters.AddWithValue("Name", marker.Name);
 
-        return new MarkerModel{ Id = markerId, Name = marker.Name };
+        using var reader = await cmd.ExecuteReaderAsync(System.Data.CommandBehavior.SingleResult);
+        
+        if (await reader.ReadAsync())
+        {
+            var markerId = reader.GetInt32(0);
+            return new MarkerModel { Id = markerId, Name = marker.Name };
+        }
+        else
+        {
+            return Result.Failure<MarkerModel>(MarkerErrors.MarkerNotUniqueError);
+        }
     }
 
     public async Task<Result> DeleteMarker(long id)
@@ -56,7 +63,7 @@ public class MarkerRepositoryPg : PgRepository, IMarkerRepository
 
         var result = await command.ExecuteNonQueryAsync();
 
-        return result == 0 ? Result.Failure(Error.NullValue) : Result.Success();
+        return result == 0 ? Result.Failure(MarkerErrors.MarkerNotFoundError) : Result.Success();
     }
 
     public async Task<Result<MarkerModel>> GetMarkerById(long id)
@@ -70,16 +77,23 @@ public class MarkerRepositoryPg : PgRepository, IMarkerRepository
         """;
 
         await using var connection = await GetConnection();
-        MarkerModel? result = await connection.QuerySingleOrDefaultAsync<MarkerModel>(
-            new CommandDefinition(
-                sqlGetMarkerById,
-                new {
-                    Id = id
-                }
-            )
-        );
 
-        return result;
+        using var cmd = new NpgsqlCommand(sqlGetMarkerById, connection);
+        cmd.Parameters.AddWithValue("Id", id);
+
+        var reader = await cmd.ExecuteReaderAsync();
+
+        if (await reader.ReadAsync())
+        {
+            var markerId = reader.GetInt32(reader.GetOrdinal("id"));
+            var markerName = reader.GetString(reader.GetOrdinal("name"));
+
+            return new MarkerModel { Id = markerId, Name = markerName };
+        }
+        else
+        {
+            return Result.Failure<MarkerModel>(MarkerErrors.MarkerNotFoundError);
+        }
     }
 
     public async Task<Result<IEnumerable<MarkerModel>>> GetMarkers()
@@ -92,15 +106,24 @@ public class MarkerRepositoryPg : PgRepository, IMarkerRepository
         """;
 
         await using var connection = await GetConnection();
-        IEnumerable<MarkerModel> result = await connection.QueryAsync<MarkerModel>(
-            new CommandDefinition(
-                sqlGetMarkers
-            )
-        );
+        using var cmd = new NpgsqlCommand(sqlGetMarkers, connection);
+    
+        using var reader = await cmd.ExecuteReaderAsync();
+        var markers = new List<MarkerModel>();
 
-        return Result.Success(result);
+        while (await reader.ReadAsync())
+        {
+            var marker = new MarkerModel
+            {
+                Id = reader.GetInt32(reader.GetOrdinal("id")),
+                Name = reader.GetString(reader.GetOrdinal("name"))
+            };
+
+            markers.Add(marker);
+        }
+
+        return markers;
     }
-
     public async Task<Result<MarkerModel>> UpdateMarker(MarkerModel marker)
     {
         const string sqlUpdateMarker = 
@@ -112,16 +135,19 @@ public class MarkerRepositoryPg : PgRepository, IMarkerRepository
         """;
 
         await using var connection = await GetConnection();
+        using var cmd = new NpgsqlCommand(sqlUpdateMarker, connection);
 
-        long id = await connection.QuerySingleOrDefaultAsync<long>(
-            new CommandDefinition(
-                sqlUpdateMarker,
-                marker
-            )
-        );
+        cmd.Parameters.AddWithValue("Name", marker.Name);
+        cmd.Parameters.AddWithValue("Id", marker.Id);
 
-        if (id==marker.Id){
-            return marker;
+        var returnedId = await cmd.ExecuteScalarAsync();
+
+        if (returnedId != null && (long)returnedId == marker.Id)
+        {
+            return new MarkerModel{
+                Id = marker.Id,
+                Name = marker.Name
+            };
         }
 
         return Result.Failure<MarkerModel>(Error.NullValue);
