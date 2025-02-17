d0a523178e59c978fe1e991f8927c2ad58c2c8ac
homyachim <homeliyanskaya@gmail.com>
2025-02-17 13:48:43 +0300

    Add annotations for Label

--- 251001/Homeliyanskaya/user-stories/src/main/java/com/homel/user_stories/model/Label.java
+++ 251001/Homeliyanskaya/user-stories/src/main/java/com/homel/user_stories/model/Label.java
@@ -1,5 +1,13 @@
 package com.homel.user_stories.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,11 +20,21 @@ import java.util.Set;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "tbl_label", schema = "distcomp")
 public class Label {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
 
+    @ManyToMany
+    @JoinTable(
+            name = "label_story", // имя таблицы связи
+            joinColumns = @JoinColumn(name = "label_id"), // столбец для id метки
+            inverseJoinColumns = @JoinColumn(name = "story_id") // столбец для id истории
+    )
     private Set<Story> stories = new HashSet<>();
 }
