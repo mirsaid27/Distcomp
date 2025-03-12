cac00a6adf7a4df5c58ccb247261e7e3ced83eed
Egor Pankratiev <egor.pankratiew@gmail.com>
2025-03-12 21:32:24 +0300

    configured PostgreSQL integration with Spring Data JP

--- 251003/Pankratiev/rest/build.gradle.kts
+++ 251003/Pankratiev/rest/build.gradle.kts
@@ -26,6 +26,8 @@ dependencies {
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	runtimeOnly("org.postgresql:postgresql:42.7.2")
 }
 
 kotlin {
