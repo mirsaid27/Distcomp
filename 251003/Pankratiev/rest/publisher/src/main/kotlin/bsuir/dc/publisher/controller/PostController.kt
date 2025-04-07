1b84f12335d4df8eee65009b3f04612e445f0d0b
Egor Pankratiev <egor.pankratiew@gmail.com>
2025-04-07 23:07:50 +0300

    added rest communication between services

--- 251003/Pankratiev/rest/publisher/src/main/kotlin/bsuir/dc/publisher/controller/PostController.kt
+++ 251003/Pankratiev/rest/publisher/src/main/kotlin/bsuir/dc/publisher/controller/PostController.kt
@@ -7,16 +7,19 @@ import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import org.springframework.web.reactive.function.client.WebClient
 
 @RestController
 @RequestMapping("/api/v1.0/posts")
 class PostController(
-    private val postService: PostService
+    private val postService: PostService,
+    private val webClient: WebClient
 ) {
 
     @PostMapping
     fun createPost(@RequestBody @Valid postRequestTo: PostRequestTo): ResponseEntity<PostResponseTo> {
         val createdPost = postService.createPost(postRequestTo)
+        sendPostRequest(createdPost)
         return ResponseEntity(createdPost, HttpStatus.CREATED)
     }
 
@@ -35,11 +38,13 @@ class PostController(
     @PutMapping()
     fun updatePost(@RequestBody @Valid postRequestTo: PostRequestTo): ResponseEntity<PostResponseTo> {
         val updatedPost = postService.updatePost(postRequestTo.id, postRequestTo)
+        sendPutRequest(updatedPost)
         return ResponseEntity.ok(updatedPost)
     }
 
     @DeleteMapping("/{id}")
     fun deletePost(@PathVariable id: Long): ResponseEntity<Void> {
+        sendDeleteRequest(id)
         postService.deletePost(id)
         return ResponseEntity.noContent().build()
     }
@@ -48,5 +53,33 @@ class PostController(
     fun getPostsByIssueId(@PathVariable issueId: Long): ResponseEntity<List<PostResponseTo>> {
         val posts = postService.getPostsByIssueId(issueId)
         return ResponseEntity.ok(posts)
+    }
+
+    private val url = "http://localhost:24130/api/v1.0/posts"
+
+    fun sendPostRequest(body: PostResponseTo) {
+        webClient.post()
+            .uri(url)
+            .bodyValue(body)
+            .retrieve()
+            .toBodilessEntity()
+            .subscribe()
+    }
+
+    fun sendPutRequest(body: PostResponseTo) {
+        webClient.put()
+            .uri(url)
+            .bodyValue(body)
+            .retrieve()
+            .toBodilessEntity()
+            .subscribe()
+    }
+
+    fun sendDeleteRequest(id: Long) {
+        webClient.delete()
+            .uri("$url/$id")
+            .retrieve()
+            .toBodilessEntity()
+            .subscribe()
     }
 }
