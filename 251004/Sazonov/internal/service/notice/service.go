9cba929da506e1843d0a7f7b76a9d0c1b4449b3c
sazonovItas <itassudo@gmail.com>
2025-04-19 11:56:40 +0300

    feature: notice caching with redis

--- 251004/Sazonov/internal/service/notice/service.go
+++ 251004/Sazonov/internal/service/notice/service.go
@@ -4,8 +4,6 @@ import (
 	"context"
 
 	"github.com/Khmelov/Distcomp/251004/Sazonov/internal/model"
-	"github.com/Khmelov/Distcomp/251004/Sazonov/pkg/logger"
-	"go.uber.org/zap"
 )
 
 type newsRepo interface {
@@ -24,17 +22,30 @@ type asyncNotice interface {
 	CreateNoticeAsync(ctx context.Context, args model.Notice) error
 }
 
+type noticeCache interface {
+	UpdateNoticeCache(ctx context.Context, notice model.Notice) error
+	GetNoticeFromCache(ctx context.Context, id int64) (model.Notice, error)
+	DeleteNoticeFromCache(ctx context.Context, id int64) error
+}
+
 type noticeService struct {
 	syncNotice
 	asyncNotice
-	news newsRepo
+	news  newsRepo
+	cache noticeCache
 }
 
-func New(syncNotice syncNotice, asyncNotice asyncNotice, news newsRepo) *noticeService {
+func New(
+	syncNotice syncNotice,
+	asyncNotice asyncNotice,
+	news newsRepo,
+	cache noticeCache,
+) *noticeService {
 	return &noticeService{
 		syncNotice:  syncNotice,
 		asyncNotice: asyncNotice,
 		news:        news,
+		cache:       cache,
 	}
 }
 
@@ -63,9 +74,51 @@ func (n *noticeService) CreateNotice(ctx context.Context, args model.Notice) (mo
 
 	notice, err := n.syncNotice.CreateNotice(ctx, args)
 	if err != nil {
-		logger.Error(ctx, "create notice", zap.Error(err))
+		return model.Notice{}, err
+	}
+
+	if err := n.cache.UpdateNoticeCache(ctx, notice); err != nil {
 		return model.Notice{}, err
 	}
 
 	return notice, nil
+}
+
+func (n *noticeService) GetNotice(ctx context.Context, id int64) (model.Notice, error) {
+	notice, err := n.cache.GetNoticeFromCache(ctx, id)
+	if err == nil {
+		return notice, nil
+	}
+
+	notice, err = n.syncNotice.GetNotice(ctx, id)
+	if err != nil {
+		return model.Notice{}, err
+	}
+
+	return notice, nil
+}
+
+func (n *noticeService) UpdateNotice(ctx context.Context, args model.Notice) (model.Notice, error) {
+	notice, err := n.syncNotice.UpdateNotice(ctx, args)
+	if err != nil {
+		return model.Notice{}, err
+	}
+
+	if err := n.cache.UpdateNoticeCache(ctx, notice); err != nil {
+		return model.Notice{}, nil
+	}
+
+	return notice, nil
+}
+
+func (n *noticeService) DeleteNotice(ctx context.Context, id int64) error {
+	if err := n.syncNotice.DeleteNotice(ctx, id); err != nil {
+		return err
+	}
+
+	if err := n.cache.DeleteNoticeFromCache(ctx, id); err != nil {
+		return err
+	}
+
+	return nil
 }
