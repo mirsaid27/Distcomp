ffa2b753795c7d719ba792de1e4e9948f589a5c5
Egor Pankratiev <egor.pankratiew@gmail.com>
2025-04-27 20:00:34 +0300

    added Redis caching support with configuration and updated application properties

--- 251003/Pankratiev/rest/publisher/build.gradle.kts
+++ 251003/Pankratiev/rest/publisher/build.gradle.kts
@@ -2,4 +2,6 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	runtimeOnly("org.postgresql:postgresql:42.7.2")
 	testRuntimeOnly("com.h2database:h2:2.2.224")
+	implementation("org.springframework.boot:spring-boot-starter-data-redis")
+	implementation("org.springframework.boot:spring-boot-starter-cache")
 }
