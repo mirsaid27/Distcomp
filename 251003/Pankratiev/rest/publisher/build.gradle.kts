5ba749514fdd7aed224917c3e951cc8f6ff34db3
Egor Pankratiev <egor.pankratiew@gmail.com>
2025-04-28 14:46:01 +0300

    updated docker-compose to include Redis and replace Redis with Cassandra in service dependencies

--- 251003/Pankratiev/rest/publisher/build.gradle.kts
+++ 251003/Pankratiev/rest/publisher/build.gradle.kts
@@ -1,7 +1,8 @@
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	runtimeOnly("org.postgresql:postgresql:42.7.2")
-	testRuntimeOnly("com.h2database:h2:2.2.224")
 	implementation("org.springframework.boot:spring-boot-starter-data-redis")
 	implementation("org.springframework.boot:spring-boot-starter-cache")
+
+	testRuntimeOnly("com.h2database:h2:2.2.224")
 }
